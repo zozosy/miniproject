9546b853c7be62c25bec6b442a815329f69be25d
zozosy <zoubaida.taher17@gmail.com>
2024-02-17 18:10:41 -0800

    initialized Sequelize with Postgres DB variables

---
+++ config/sequelize.js
@@ -0,0 +1,7 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('assignment2_4ewv', 'assignment2_4ewv_user', 'vsPEQZTEyJn2ZGr3BGV5zSQKHrnG0AK8', {
+  host: 'dpg-cmu5tguv3ddc738f77jg-a',
+  port: 5432,
+  dialect: 'postgres',
+});
