1a58aae50d09cad65d63d9080b9c9bd23a0baba4
zozosy <zoubaida.taher17@gmail.com>
2024-02-27 18:47:12 -0800

    client add

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+    <div>
+      <p> This is your starting parent component. Feel free to add additional components inside of the components directory. In addition, we provided utils & services directory for a reason. 
+        You can add additional functions to the utils that don't need to exist inside of the components. Furthermore, the services is there to make that connection
+        to your server endpoints!
+      </p>
+    </div>
+  )
+}
+
+export default App
